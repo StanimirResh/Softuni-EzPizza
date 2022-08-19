a0b6ada2bfb2f02e278a34f9572bc8a804d79434
StanimirResh <mchorko@gmail.com>
2022-08-20 01:12:00 +0300

    user service minor fix

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,12 +3,8 @@ import * as request from './requester'
 const baseUrl = "http://localhost:3030/users"
 
 export const login = async (email, password) => {
-    try {
-        const response = await request.post(`${baseUrl}/login`, { email, password })
-        return response
-    } catch (err) {
-        throw err
-    }
+    const response = await request.post(`${baseUrl}/login`, { email, password })
+    return response
 }
 
 export const register = (email, password) => {
