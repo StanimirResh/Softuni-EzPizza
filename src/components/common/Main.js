819a5d3f75be9b2673ba6c159d11816549d472a8
StanimirResh <mchorko@gmail.com>
2022-08-21 01:53:08 +0300

    minor improvements in the main component

--- src/components/common/Main.js
+++ src/components/common/Main.js
@@ -13,21 +13,28 @@ import { Create } from "../Menu/Create/Create"
 import { Delete } from "../Menu/Details/Delete/Delete"
 
 export const Main = () => {
+    const communityDb = 'community-pizzas'
+    const basicDb = 'basic-pizzas'
     return (
         <main>
+            {/* Tried using context instead of giving each component seperately which db to use but
+            the <Routes> component only accepts <Route> children. Didnt find another workaround for it */}
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
                 <Route path='/login' element={<Login />} />
                 <Route path='/register' element={<Register />} />
                 <Route path="/contacts" element={<Contacts />} />
-                <Route path="/menu/basic-pizzas" element={<Menu database="basic-pizzas" />} />
-                <Route path="/menu/community-pizzas" element={<Menu database="community-pizzas" />} />
-                <Route path="/menu/basic-pizzas/:pizzaId" element={<Details database="basic-pizzas" />} />
-                <Route path="/menu/community-pizzas/:pizzaId" element={<Details database="community-pizzas" />} />
-                <Route path="/menu/community-pizzas/:pizzaId/edit" element={<Edit database="community-pizzas" />} />
-                <Route path="/menu/community-pizzas/:pizzaId/delete" element={<Delete database="community-pizzas" />} />
-                <Route path="/menu/community-pizzas/create" element={<Create database="community-pizzas" />} />
+                <Route path="/menu/basic-pizzas" element={<Menu database={basicDb} />} />
+                <Route path="/menu/basic-pizzas/:pizzaId" element={<Details database={basicDb} />} />
+                <Route path="/menu/basic-pizzas/:pizzaId/edit" element={<Edit database={basicDb} />} />
+                <Route path="/menu/basic-pizzas/:pizzaId/delete" element={<Delete database={basicDb} />} />
+                <Route path="/menu/basic-pizzas/create" element={<Create database={basicDb} />} />
+                <Route path="/menu/community-pizzas" element={<Menu database={communityDb} />} />
+                <Route path="/menu/community-pizzas/:pizzaId" element={<Details database={communityDb} />} />
+                <Route path="/menu/community-pizzas/:pizzaId/edit" element={<Edit database={communityDb} />} />
+                <Route path="/menu/community-pizzas/:pizzaId/delete" element={<Delete database={communityDb} />} />
+                <Route path="/menu/community-pizzas/create" element={<Create database={communityDb} />} />
                 <Route path="/cart" element={<Cart />} />
                 <Route path="/logout" element={<Logout />} />
             </Routes>
