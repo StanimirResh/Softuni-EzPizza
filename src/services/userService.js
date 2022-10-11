8851204bfbd3cfa0eb06a377e4d5c60986e6db63
StanimirResh <mchorko@gmail.com>
2022-10-12 00:02:37 +0300

    Update userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,11 +1,12 @@
 import * as request from './requester'
 
-const baseUrl = "http://localhost:3030/data/users"
+const baseUrl = "http://localhost:3030/users"
 
 export const login = async (email, password) => {
     const response = await request.post(`${baseUrl}/login`, { email, password })
     return response
 }
+
 
 export const register = (email, password) => {
     return request.post(`${baseUrl}/register`, { email, password })
